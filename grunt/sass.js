01c6c76f5645a154a21427b57ea99fc53739253b
Trezy <tre@trezy.com>
2015-09-08 15:12:09 -0500

    Add Theming
    Remove theming from core styles
    Add example theme
    update grunt sass task to compile themes

--- grunt/sass.js
+++ grunt/sass.js
@@ -5,7 +5,17 @@ module.exports = {
     },
 
     files: {
-      'css/app.css': 'scss/app.scss'
+      'css/app.css': 'scss/main/app.scss'
     }
+  },
+  themes: {
+    files: [{
+      expand: true,
+      flatten: true,
+      cwd: 'scss/themes',
+      src: '**/*.scss',
+      dest: 'css/themes',
+      ext: '.css'
+    }]
   }
 }
