a4618b1bf1386ca5259c07edec2ae5d6660f2c24
Trezy <tre@trezy.com>
2015-09-22 19:05:46 -0500

    Adding some basic windows parameters

--- main.js
+++ main.js
@@ -10,24 +10,24 @@ var mainWindow = null;
 
 // Quit when all windows are closed.
 app.on( 'window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if ( process.platform != 'darwin' ) {
-    app.quit();
-  }
+  app.quit();
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on( 'ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow( { width: 800, height: 600 } );
+  mainWindow = new BrowserWindow({
+    'accept-first-mouse': true,
+    height: 600,
+    'min-height': 600,
+    'min-width': 800,
+    title: 'Turk',
+    width: 800
+  });
 
   // and load the index.html of the app.
   mainWindow.loadUrl( 'file://' + __dirname + '/index.html' );
-
-  // Open the DevTools.
-  mainWindow.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on( 'closed', function() {
